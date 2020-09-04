7f3585c67e933f3760d8407d45257c6243b2c741
Higor Borges Martins <higorbm1@gmail.com>
2020-09-04 13:14:11 -0300

    teste

--- src/treinoestudo/TreinoEstudo.java
+++ src/treinoestudo/TreinoEstudo.java
@@ -16,6 +16,7 @@ public class TreinoEstudo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello Word");
     }
     
 }
